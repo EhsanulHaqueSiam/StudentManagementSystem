ab9c2bc853e55a50c60fea13c6029dc0ec020f2a
Md Ehsanul Haque <133691757+EhsanulHaqueSiam@users.noreply.github.com>
2023-07-29 19:13:54 +0600

    Delete CourseController.java

--- src/main/java/com/studentmanagementsystem/controller/CourseController.java
+++
@@ -1,4 +0,0 @@
-package main.java.com.studentmanagementsystem.controller;
-//this class is for managing courses . like creating , deleting , renaming , assigning.
-
-public class CourseController {}
